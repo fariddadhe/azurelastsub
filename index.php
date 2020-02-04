a3a10cefb58fee2485ff6bd64f9ab3c7b764d86c
fariddadhe <60096170+fariddadhe@users.noreply.github.com>
2020-02-04 16:51:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,111 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Analyze Sample</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    </head>
+
     
+
+    
+    <!-- <body onload="processImage()">
+    <div id="process"></div>    -->
+
+    <body>
+    
+
+
+    <?php if (isset($result)) { ?>
+        <h1> Result: <?php echo $result ?></h1>
+    <?php } ?>
+    <form action="" method="post" enctype="multipart/form-data">
+        <div>
+            <label for="file">Choose file to analyze</label>
+            <input type="file" id="fileToUpload" name="fileToUpload">
+        </div>
+        <div>
+        <input type="submit" value="Upload Image" name="submit">
+        </div>
+    </form>
+    <!-- <input type="file" id="fileToUpload" name="fileToUpload">
+    <button onclick="data()">Submit</button> -->
+   
+    
+    
+  
+    <br><br>
+    <div id="wrapper" style="width:1020px; display:table;">
+        <div id="jsonOutput" style="width:600px; display:table-cell;">
+            Response:
+            <br><br>
+            <textarea id="responseTextArea" class="UIInput"
+                      style="width:580px; height:400px;"></textarea>
+        </div>
+        <div id="imageDiv" style="width:420px; display:table-cell;">
+            Source image:
+            <br><br>
+            <img id="sourceImage" width="400" />
+        </div>
+    </div>
+    </body>
+    </html>
+
+<script type="text/javascript">
+    
+    function processImage(data) {
+
+        var data = data;
+
+        console.log("data :" + data);
+        var subscriptionKey = "b696bf5121d04757a2063556aa701083";
+ 
+
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        
+        document.querySelector("#sourceImage").src = data;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + data + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
     <?php
 
     require_once 'vendor/autoload.php';
@@ -10,9 +117,7 @@
     use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
     use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
     function getData(){
-        
-        $connectionString = "DefaultEndpointsProtocol=https;AccountName=azurewebblob;AccountKey=IRtUInLb2Cl8bYEp54JjFOsPTd9+eNdytEprSfLVTZWQKPp6i668ez1iU3h99iZRBjpqSsxwBi4dwHv7UvHLXg==";
-        
+       $connectionString = "DefaultEndpointsProtocol=https;AccountName=azurewebblob;AccountKey=IRtUInLb2Cl8bYEp54JjFOsPTd9+eNdytEprSfLVTZWQKPp6i668ez1iU3h99iZRBjpqSsxwBi4dwHv7UvHLXg==";
 
         // Create blob client.
         $blobClient = BlobRestProxy::createBlobService($connectionString);
@@ -98,113 +203,13 @@
             }
         }
         return $url;
+        
     }
 
     if(isset($_POST['submit'])){
         $result = getData();
+        echo '<script type="text/javascript"> processImage("'.$result.'"); </script>';
+        
     }
     ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Analyze Sample</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    </head>
-
-    
-
-    <script type="text/javascript">
-        function processImage() {
-    
-            var subscriptionKey = "b696bf5121d04757a2063556aa701083";
-     
-    
-            var uriBase =
-                "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-     
-            // Request parameters.
-            var params = {
-                "visualFeatures": "Categories,Description,Color",
-                "details": "",
-                "language": "en",
-            };
-     
-            // Display the image.
-            var sourceImageUrl = document.getElementById("inputImage").value;
-            document.querySelector("#sourceImage").src = sourceImageUrl;
-     
-            // Make the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-     
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader(
-                        "Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-     
-                type: "POST",
-     
-                // Request body.
-                data: '{"url": ' + '"' + sourceImageUrl + '"}',
-            })
-     
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-            })
-     
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ? "Error. " :
-                    errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ? "" :
-                    jQuery.parseJSON(jqXHR.responseText).message;
-                alert(errorString);
-            });
-        };
-    </script>
-    <body onload="processImage()">
-    <div id="process"></div>   
-
-
-    <?php if (isset($result)) { ?>
-        <h1> Result: <?php echo $result ?></h1>
-    <?php } ?>
-    <form action="" method="post" enctype="multipart/form-data">
-        <div>
-            <label for="file">Choose file to analyze</label>
-            <input type="file" id="fileToUpload" name="fileToUpload">
-        </div>
-        <div>
-        <input type="submit" value="Upload Image" name="submit">
-        </div>
-    </form>
    
-    
-    
-     
-    <h1>Analyze image:</h1>
-    Enter the URL to an image, then click the <strong>Analyze image</strong> button.
-    <br><br>
-    Image to analyze:sdfsdf
-    <input type="text" name="inputImage" id="inputImage"
-        value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-    <button onclick="processImage()">Analyze image</button>
-    <br><br>
-    <div id="wrapper" style="width:1020px; display:table;">
-        <div id="jsonOutput" style="width:600px; display:table-cell;">
-            Response:
-            <br><br>
-            <textarea id="responseTextArea" class="UIInput"
-                      style="width:580px; height:400px;"></textarea>
-        </div>
-        <div id="imageDiv" style="width:420px; display:table-cell;">
-            Source image:
-            <br><br>
-            <img id="sourceImage" width="400" />
-        </div>
-    </div>
-    </body>
-    </html>
